bf9a829ab87b0e7aa04c55eadd116d323f8e632a
yuval <yuval@bar-ilan>
2014-09-02 13:35:51 +0300

    don't remember exactly

--- matlab/G2.m
+++ matlab/G2.m
@@ -1,14 +1,18 @@
 function kurt=G2(x)
 % x is a data matrix, raws for channels, columns for time samples.
+ind=find(sum(x'));% looking for zero channels to exclude
+sizeX=size(x);
+x=x(ind,:);
 xbl=x-repmat(mean(x,2),1,size(x,2));
 x2=xbl.^2;
 x4=xbl.^4;
 sx2=sum(x2')';
 sx4=sum(x4')';
 VAR=sx2./(size(x,2)-1);
-if ~isempty(find(VAR.*VAR==0)) 
-    kurt=G2(1000.*x);
+kurt=zeros(sizeX(1),1);
+if ~isempty(find(VAR.*VAR==0))
+    kurt(ind)=G2(1000.*x);
     warning('Multiplying x by 1000, VAR^2 is too small for single. consider rescaling X before using G2')
 else
-kurt=sx4./(VAR.*VAR.*size(x,2))-3;
+    kurt(ind)=sx4./(VAR.*VAR.*size(x,2))-3;
 end
